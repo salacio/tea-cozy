abf9acc08fc3b390420127ca99a5357e0d3c34ea
David <dgs84@pm.me>
2023-08-01 18:55:49 -0500

    tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
-  theme: {
-    colors: {
-      "seashell": "seashell",
-      "black": "black",
-    },
-    fontFamily: {
-      "helvetica": "helvetica",
-    },
-    extend: {
-      backgroundImage: {
-        "mission": "url('/src/img-mission-background.jpg')",
-        "locations": "url('/src/img-locations-background.jpg')",
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js}", "./index.html"],
+	theme: {
+		colors: {
+			seashell: "seashell",
+			black: "black",
+		},
+		fontFamily: {
+			helvetica: "helvetica",
+		},
+		extend: {
+			backgroundImage: {
+				mission: "url('../src/img-mission-background.jpg')",
+				locations: "url('../src/img-locations-background.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
